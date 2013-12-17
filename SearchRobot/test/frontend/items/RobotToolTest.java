16461a791891f42f3a047886b4709066831e15ba
gfels4 <simon.gfeller@gmail.com>
2013-12-17 16:54:47 +0100

    tests

---
+++ SearchRobot/test/frontend/items/RobotToolTest.java
@@ -0,0 +1,14 @@
+package frontend.items;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RobotToolTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
